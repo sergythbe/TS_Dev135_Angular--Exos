2445e3ddccb549a969198ce8bb3c046231b04734
sani serge <sergyth@gmail.com>
2026-02-12 22:52:56 +0100

    ajout du composant profil-dynamique

---
+++ src/app/features/exercices/profil-dynamique/profil-dynamique.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-dynamique',
+  imports: [],
+  templateUrl: './profil-dynamique.html',
+  styleUrl: './profil-dynamique.css',
+})
+export class ProfilDynamique {
+
+}
